a560554596cfb43c391fa1cceb57166f43a2fcac
Yan Lyra <yanlyra3@gmail.com>
2023-08-05 23:34:33 -0300

    chore: cleaned the application to start again

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,25 +1,11 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useSelector } from 'react-redux'
 
-const todoSlice = createSlice({
-  name: 'todo',
-  initialState: ['Jantar', 'Programar'],
-
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo)
-    }
-  }
-})
-
 export const store = configureStore({
-  reducer: {
-   todo: todoSlice.reducer 
-  }
+  reducer: {}
 })
 
 export type RootState = ReturnType<typeof store.getState>
 
-export const { add } = todoSlice.actions
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
